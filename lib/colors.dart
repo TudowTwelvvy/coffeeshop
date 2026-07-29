7826c4d14503db34b1a6d8072a44e5c019ad2240
Tumelo Khanye <tumelokhanye9811@gmail.com>
2026-07-29 12:14:05 +0200

    Add initial implementation of splash screen and common button widget, along with color definitions and home screen setup

---
+++ lib/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color xbgColor = const Color(0xfff9f9f9);
+Color xprimaryColor = const Color(0xffC67C4E);
+Color xsecColor = const Color(0xffA2A2A2);
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,67 @@
+import 'package:coffeeshop/Widgets/common_button.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SizedBox(
+        height: size.height,
+        width: size.width,
+        child: Stack(
+          children: [
+            SizedBox(
+              child: Image.asset(
+                'assets/images/bg.png',
+                height: size.height / 1.3,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Positioned(
+              bottom: 45,
+              right: 0,
+              left: 0,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+
+                  children: [
+                    Text(
+                      "Fall in Love with Coffe in Blissful Delight!",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 35,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        height: 1.3,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      "Welcome to our cozy coffee corner, where every cup is a delightful experience",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.grey[600],
+                        fontWeight: FontWeight.w500,
+                        height: 1.3,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    CommonButton(title: "Get Started", onTab: () {}),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
